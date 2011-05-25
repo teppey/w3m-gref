488d21a640a3cdffce735a3a455b2a8b4c731d12
Teppei Hamada <temada@gmail.com>
2011-05-25 22:16:25 +0900

    initial commit

---
+++ setup.scm
@@ -0,0 +1,205 @@
+(use dbm)
+(use dbm.gdbm)
+(use file.util)
+(use gauche.parseopt)
+(use gauche.process)
+(use gauche.record)
+(use rfc.http)
+(use rfc.uri)
+(use srfi-1)
+(use srfi-13)
+(use util.list)
+(use util.match)
+
+(define-record-type (topic (pseudo-rtd <vector>)) #t #f
+  key href section)
+
+(define *archives*
+  '((en . "http://practical-scheme.net/vault/gauche-refe.tgz")
+    (ja . "http://practical-scheme.net/vault/gauche-refj.tgz")))
+
+(define (main args)
+  (let-args (cdr args)
+    ((html-dir  "d|html-dir=s")
+     (lang      "l|lang=y" 'ja)
+     (help      "h|help" => (cut usage))
+     (else  =>  (^(option rest cont)
+                  (print "unknown option: " option)
+                  (usage))))
+
+    (unless (file-exists? (app-directory))
+      (create-app-directory))
+
+    (cond ((and (not html-dir)
+                (assq-ref *archives* lang))
+           => (lambda (archive-uri)
+                (set! html-dir (download-reference archive-uri (app-directory)))))
+          ((and html-dir (relative-path? html-dir)
+           (set! html-dir
+             (sys-normalize-pathname
+               html-dir :absolute #t :expand #t :canonicalize #t)))))
+
+    (unless (and html-dir
+                 (absolute-path? html-dir)
+                 (file-is-directory? html-dir))
+      (error "not directory:" html-dir))
+
+    (make-index html-dir)
+
+    (write-config (app-directory)
+                  `((html-dir . ,html-dir)
+                    (db-path  . ,(db-path))))
+    0))
+
+(define (usage)
+  (for-each print (list #`"usage: gosh ,(sys-basename *program-name*) [options]"
+                        "options:"
+                        "\t-d, --html-dir\tGauche reference directory"
+                        "\t-l, --lang\tlanguage of reference manual (en|ja)"
+                        "\t-h, --help\tshow this message"))
+            (exit 0))
+
+(define (app-directory)
+  (build-path (home-directory) ".w3m-gref"))
+
+(define (db-path)
+  (build-path (app-directory) "gref.dbm"))
+
+(define (create-app-directory)
+  (make-directory* (app-directory) #o755))
+
+(define (split-uri uri)
+  (receive (scheme specific)
+    (uri-scheme&specific uri)
+    (receive (authority path query fragment)
+      (uri-decompose-hierarchical specific)
+      (lambda (message)
+        (case message
+          ((scheme) scheme)
+          ((authority) authority)
+          ((path) path)
+          ((query) query)
+          ((fragment) fragment)
+          (else (error "split-uri - unknown message:" message)))))))
+
+  (define (path->html-dir path)
+    (car (string-split (regexp-replace #/^.*\// path "") ".")))
+
+(define (download-reference uri extract-dir)
+  (let ((uri (split-uri uri))
+        (proxy (and-let* ((env (sys-getenv "http_proxy")))
+                 (split-uri env))))
+    (receive (status hdrs body)
+      (call-with-output-process `(tar xzf - -C ,extract-dir)
+        (lambda (sink)
+          (http-get (uri 'authority) (uri 'path)
+                    :proxy (and proxy (proxy 'authority))
+                    :sink sink
+                    :flusher (lambda (sink headers)
+                               (close-output-port sink)))))
+      (unless (string=? status "200")
+        (error "download failed. Server returns a" status))
+      (build-path extract-dir (path->html-dir (uri 'path))))))
+
+(define (make-index html-dir)
+  (let* ((htmls (get-htmls html-dir))
+         (topics (append (append-map get-topics (get-index-htmls htmls))
+                         (get-section-topics (toc-file html-dir))))
+         (db (dbm-open <gdbm> :path (db-path) :rw-mode :create :value-convert #t)))
+
+           ;; html -> title
+           (for-each (^(html)
+                       (dbm-put! db (sys-basename html) (html-title html)))
+                     htmls)
+
+           ;; topic -> (href ...)
+           (for-each (match-lambda
+                       ((key . hrefs) (dbm-put! db key hrefs)))
+                     (merge-topics topics))
+
+           (dbm-close db)
+           )
+  )
+
+(define (topic=? t1 t2)
+  (and (string=? (topic-key t1) (topic-key t2))
+       (topic-section t1)
+       (topic-section t2)
+       (string=? (topic-section t1) (topic-section t2))))
+
+;; (topic ...) -> ((topic-key . (topic-href ...)) ...)
+(define (merge-topics topics)
+  (let ((ht (make-hash-table 'string=?)))
+    (dolist (t topics)
+      (hash-table-update!
+        ht (topic-key t)
+        (^(lst) (if lst (delete-duplicates (cons t lst) topic=?) (list t)))
+        #f))
+    (hash-table-map ht (lambda (key topics)
+                         (cons key (map topic-href topics))))))
+
+(define (get-htmls dir)
+  (glob (build-path dir "gauche-ref[ej]*.html")))
+
+(define (toc-file dir)
+  (car (glob (build-path dir "gauche-ref[ej]_toc.html"))))
+
+(define (index-html? html)
+  (with-input-from-file html
+    (lambda ()
+      (let loop ((line (read-line)))
+        (cond ((eof-object? line) #f)
+              ((string-scan line "class=\"summary-letter\"") #t)
+              (else (loop (read-line))))))
+    :encoding "*jp"))
+
+(define (get-index-htmls htmls)
+  (filter index-html? htmls))
+
+(define (html-title html)
+  (with-input-from-file html
+    (lambda ()
+      (let loop ((line (read-line)))
+        (cond [(eof-object? line) #f]
+              [(#/<title>(.*?)<\/title>/i line)
+               => (lambda (m)
+                    (string-trim-both
+                      (or (string-scan (m 1) #\: 'after) m)))]
+              [else (loop (read-line))])))
+    :encoding "*jp"))
+
+(define (html-unescape s)
+  (regexp-replace-all* s
+    #/&gt\;/   ">"
+    #/&lt\;/   "<"
+    #/&quot\;/ "\""
+    #/&amp\;/  "&"))
+
+(define (tag-remove s)
+  (regexp-replace-all #/<\/?\w+?>/ s ""))
+
+(define (grep regexp proc :optional (port (current-input-port)))
+  (filter-map (^(line)
+                (and-let* ((m (rxmatch regexp line)))
+                  (proc m)))
+              (port->string-list port)))
+
+(define (get-topics index-html)
+  (define rx #/<a href=\"(?<href>.+?)\"><code>(?<key>.+?)<\/code>.*<a href=\".+?\">(?<section>.+?)<\/a>/i)
+  (with-input-from-file index-html
+    (cut grep rx (^m (make-topic (html-unescape (m 'key))
+                                 (m 'href)
+                                 (tag-remove (html-unescape (m 'section))))))
+    :encoding "*jp"))
+
+(define (get-section-topics toc-html)
+  ;; from: http://gauche.svn.sourceforge.net/viewvc/gauche/Gauche-scripts/trunk/gaucheref.cgi
+  (define rx #/<a name=\"[^\"]*\" href=\"(?<href>[^\"]*)\">\d\.[.\d]*\s+(?<key>[^<]*)<\/a>/i)
+  (with-input-from-file toc-html
+    (cut grep rx (^m (make-topic (html-unescape (tag-remove (m 'key)))
+                                 (m 'href)
+                                 #f)))
+    :encoding "*jp"))
+
+(define (write-config dir sexp)
+  (with-output-to-file (build-path dir "config") (cute write sexp)))
---
+++ w3m-gref.cgi
@@ -0,0 +1,149 @@
+#!/usr/bin/env gosh
+;; vim:set ft=scheme fileencoding=utf-8:
+
+;;;
+;;; w3m-gref.cgi - Gaucheのリファレンスを検索するLocal CGI
+;;;
+;;; by Teppei Hamada <temada@gmail.com>
+;;;
+
+(use dbm)
+(use dbm.gdbm :only (<gdbm>))
+(use file.util :only (home-directory temporary-directory build-path))
+(use gauche.record :only (define-record-type))
+(use rfc.uri :only (uri-decode-string))
+(use srfi-1 :only (append-map))
+(use srfi-13 :only (string-null?))
+(use text.html-lite)
+(use text.tree :only (write-tree))
+
+(define *app-directory* (build-path (home-directory) ".w3m-gref"))
+
+(define config
+  (let1 alist (with-input-from-file (build-path *app-directory* "config") read)
+    (lambda (key)
+      (and-let* ((pair (assoc key alist)))
+        (cdr pair)))))
+
+(define path->url
+  (pa$ string-append "file://"))
+
+(define (root-url)
+  (path->url (car (glob (build-path (config 'html-dir) "gauche-ref[ej].html")))))
+
+(define (with-db proc)
+  (let1 db (dbm-open <gdbm> :path (config 'db-path) :rw-mode :read :value-convert #t)
+    (unwind-protect
+      (proc db)
+      (unless (dbm-closed? db)
+        (dbm-close db)))))
+
+(define (eprint content)
+  (with-output-to-file (build-path (temporary-directory) "gref-debug")
+    (lambda () (write content) (newline) (flush)))
+  content)
+
+(define-record-type item  #t #f
+  name url section)
+
+;; ---------------------------------------------------------------------
+;; Entry point
+;;
+(define (main _)
+  (guard (e (else (write-tree (error-page e))))
+    (write-tree ((.$ render sort-by-section itemize search get-query)))))
+
+(define (error-page e)
+  (html:html
+    (html:head (html:title "Error"))
+    (html:body
+      (html:h1 "Error")
+      (html:p (html-escape-string (~ e 'message))))))
+
+(define (render query items)
+  (define html-header "Content-Type: text/html; charset=UTF-8\r\n\r\n")
+  (define h html-escape-string)
+  (define (w3m-goto url)
+    (list #`"w3m-control: GOTO ,|url|\r\n"
+            "w3m-control: DELETE_PREVBUF\r\n"))
+  (define (single? l)
+    (and (not (null? l)) (null? (cdr l))))
+  (define (item->tr i)
+    (html:tr
+      (html:td (html:a :href (item-url i)
+                       #`",(h (item-name i)) "))
+      (html:td (h (item-section i)))))
+  (cond ((null? items)
+         (w3m-goto (root-url)))
+        ((single? items)
+         (w3m-goto (item-url (car items))))
+        (else
+          `(,html-header
+             ,(html:html
+                (html:body
+                  (html:h1 (h query))
+                  (html:table
+                    (map item->tr items))))))))
+
+(define (sort-by-section query items)
+  (define (section-number sec)
+    (or (and-let* ((m (#/^(\d+)\.(\d+)?/ sec))
+                   (n (if (and (m 2) (not (string-null? (m 2))))
+                        (string->number (m 2))
+                        0)))
+          (+ (* 1000 (string->number (m 1))) n))
+        0))
+  (values query
+          (sort-by items (^i (vector (section-number (item-section i))
+                                     (item-name i)))
+                   (^(v1 v2) (let ([nsec1 (~ v1 0)] [nsec2 (~ v2 0)]
+                                   [name1 (~ v1 1)] [name2 (~ v2 1)])
+                               (if (= nsec1 nsec2)
+                                 (string<=? name1 name2)
+                                 (< nsec1 nsec2)))))))
+
+(define (itemize query results)
+  (define (href->url href)
+    (path->url (build-path (config 'html-dir) href)))
+  (define (href->title href)
+    (let1 file (or (string-scan href #\# 'before) href)
+      (or (with-db (^(db) (dbm-get db file #f)))
+          file)))
+  (define (result->item pair)
+    (let ([name (car pair)] [hrefs (cdr pair)])
+      (map (^h (make-item name (href->url h) (href->title h)))
+           hrefs)))
+  (values query (append-map result->item results)))
+
+;; query -> (result ...)
+;; result : (name href1 href2 ...)
+(define (search query)
+  (rxmatch-case query
+    [#/^\/(.+[^\\])\/(i)?$/ (#f rx case-fold)  ; /regexp/[option]
+     (regexp-search rx (and case-fold (not (string-null? case-fold))))]
+    [#/^\/(.+)$/ (#f rx)                       ; /regexp
+     (regexp-search rx)]
+    [#/^(.+[^\\])\/(i)?$/ (#f rx case-fold)    ; regexp/[option]
+     (regexp-search rx (and case-fold (not (string-null? case-fold))))]
+    [else
+      (fixed-search query)]))
+
+(define (regexp-search query-rx :optional (case-fold #f))
+  (let* ((rx (string->regexp query-rx :case-fold case-fold))
+         (results (with-db
+                    (cut dbm-fold <>
+                         (^(key hrefs seed)
+                             (cond ((#/gauche-ref[ej].*?\.html/ key) seed)
+                                   ((rxmatch rx key)
+                                    (cons (cons key hrefs) seed))
+                                   (else seed))) '()))))
+    (values rx results)))
+
+(define (fixed-search query)
+  (if-let1 hrefs (with-db (^(db) (dbm-get db query #f)))
+    (values query (list (cons query hrefs)))
+    (values query '())))
+
+(define (get-query)
+  (and-let* ((query (string-scan (sys-getenv "QUERY_STRING") ":" 'after)))
+    (uri-decode-string query)))
